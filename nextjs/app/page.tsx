44e8d1711214f64864e502a65e594e60b156a3af
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-11-22 08:14:54 +0900

    step 4 bg car image

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -1,9 +1,16 @@
-import styles from "./page.module.css";
+import Image from "next/image";
 import { RankingPanel } from "./components/RankingPanel";
+import styles from "./page.module.css";
+import bgFile from "./bg/car-racing-8728909_1280.jpg";
 
-export default function Page() {
+export default function Home() {
   return (
     <div className={styles.page}>
+      <Image
+        className={styles.background}
+        src={bgFile}
+        alt="background image"
+      ></Image>
       <RankingPanel />
     </div>
   );
